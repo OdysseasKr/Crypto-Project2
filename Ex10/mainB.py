620ac8469fe25066380391291f1bf5ea01e292df
OdysseasKr <OdysseasKr@users.noreply.github.com>
2016-05-27 16:13:01 +0300

    Finish Ex10

--- Ex10/mainB.py
+++ Ex10/mainB.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import math
 from fast import fast
-from modinverse import modinverse
 
 # =================================================================
 def shanks(p, y, g):
@@ -14,10 +13,7 @@ def shanks(p, y, g):
         if L1[i] == L2[i]:
             return i*(A-1)
 
-        print(L1)
-        print(L2)
         inters = list(set(L1).intersection(L2))
-        print(inters)
         if len(inters) != 0:
             B = inters[0]
             quotient = L1.index(B)
@@ -25,7 +21,6 @@ def shanks(p, y, g):
             return A*quotient - reminder
 
 # ==================================================================
-# DRAZIWTIS POLARD
 def f(x,a,b,p,g,y):
     m = p - 1
     if 0 <= x and x < m/3:
@@ -37,7 +32,7 @@ def f(x,a,b,p,g,y):
 
 def polard(p, y, g):
     print("i\tx\ta\tb\tX\tA\tB")
-    m = p -1
+    m = p - 1
     a,b,x = 0,0,1
     X,A,B = x,a,b
 
@@ -48,40 +43,15 @@ def polard(p, y, g):
 
         print(str(i)+"\t"+str(x)+"\t"+str(a)+"\t"+str(b)+"\t"+str(X)+"\t"+str(A)+"\t"+str(B))
         if x == X and b!=B:
-            #print((g**a) * (y ** b) % p)
-            #print((g**A) * (y ** B) % p)
-
-            #lalala = modinverse(abs(b-B),m)
-            #tmp = ((A - a)/(b - B))# % p
-
-            #tmp = abs(A-a) * lalala % m
-            #print(tmp)
-            #print(findDat(a - A, B - b, p))
-            return x
-
-def findDat(A, B, p):
-    i = 1
-    while True:
-        if (i*B % p) == A:
-            return i
-        else:
-            i += 1
-
+            tmp = (abs(A - a)/abs(b - B))# % p
+            return tmp
 # ==================================================================
 
-# g^x = y (mod N)
-g = 3
-y = 4
-N = 43
+print("3^x = 2 (mod 43)")
+print(shanks(43, 2, 3))
 
-print(shanks(N, y, g))
-print(polard(N, y, g))
-"""
-a,b,x = 0,0,1
-m = 42
-for i in range(50):
-    (x,a,b) = f(x,a,b,43,3,5)
-    print(str(i)+"\t"+str(x)+"\t"+str(a)+"\t"+str(b))
-    if (x == 26):
-        print((g**a) * (y**b) % m)
-"""
+print("3^x = 4 (mod 43)")
+print(shanks(43, 4, 3))
+
+print("3^x = 5 (mod 43)")
+print(shanks(43, 5, 3))
