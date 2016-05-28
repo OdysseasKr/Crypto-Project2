9f5d8571ef9efc30721c9551569d79a1323df6f4
Kostis-S-Z <Kostis-S-Z@users.noreply.github.com>
2016-05-28 22:23:15 +0300

    Update CRT

--- Ex8/CRT.py
+++ Ex8/CRT.py
@@ -5,25 +5,25 @@
 
 def calculate(m1, m2, m3, n1, n2, n3):
     """Calculate CRT."""
-    M = m1 * m2 * m3  #
+    M = m1 * m2 * m3 
 
-    M1 = M / m1  #
+    M1 = M / m1 
 
-    M2 = M / m2  #
-
-    M3 = M / m3  #
+    M2 = M / m2
+    
+    M3 = M / m3
 
     # ui * Mi = 1 mod mi
-    u1 = modinverse(M1, m1)  #
+    u1 = modinverse(M1, m1)
 
-    u2 = modinverse(M2, m2)  #
+    u2 = modinverse(M2, m2)
 
-    u3 = modinverse(M3, m3)  #
+    u3 = modinverse(M3, m3)
 
-    x = (n1 * u1 * M1) + (n2 * u2 * M2) + (n3 * u3 * M3)  #
+    x = (n1 * u1 * M1) + (n2 * u2 * M2) + (n3 * u3 * M3)
 
     # answer = x mod M
-    answer = x % M  #
+    answer = x % M
 
     return answer
 
